c1617b7a93f6bb7ec295abeefe7821150047ba36
anaomart <anaomart@gmail.com>
2023-05-21 15:07:13 +0300

    fix z index in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
         variants={variants}
         // animate="end"
         // initial="start"
-        className={` absolute right-8 top-10 z-50 mx-10 flex flex-col
+        className={` absolute right-8 top-10 z-[1000] mx-10 flex flex-col
             items-center gap-8 rounded-2xl bg-white p-12 text-center  text-gray-400
            transition-all  duration-500 md:relative md:right-0 md:top-0
                         md:flex md:flex-row md:bg-transparent md:p-0 md:text-secondary ${
@@ -35,7 +35,7 @@ export default function Navbar() {
       >
         {NavLinks.map((nav) => (
           <a
-            className="z-[1000] hover:cursor-pointer hover:text-black md:hover:text-white"
+            className="z-[999] hover:cursor-pointer hover:text-black md:hover:text-white"
             href={nav.path}
             key={nav.title + Math.random()}
           >
